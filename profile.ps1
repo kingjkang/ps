301244c8bfeab7338d2c96276a163eda04b98de5
justin <justin.d.kang@gmail.com>
2018-04-16 08:49:50 -0700

    added ip6

--- profile.ps1
+++ profile.ps1
@@ -338,9 +338,19 @@ function testGnarly{
     foreach($currHost in $hosts){
 
         #this is to get adapter, ipaddress, subnetmask, defaultgateway, vlanid
-        $cnet = $currHost | Get-VMHostNetworkAdapter | select name, ip, subnetmask
+        $cnet = $currHost | Get-VMHostNetworkAdapter | select name, ip, subnetmask, ipv6
         $nets = $cnet | Where-Object {$_.IP -ne ""} 
         $mgmtIP = $nets | Where-Object {$_.name -eq "vmk0"}
+        
+        $ip6 = $mgmtIP.ipv6 | select address -ExpandProperty address
+        if($ip6.length -eq 2){
+            $primaryip6 = $ip6[0]
+            $secondaryip6 = $ip6[1]
+        } else {
+            $primaryip6 = $ip6
+            $secondaryip6 = " "
+        }
+
         $defaultGateway = $currHost.ExtensionData.Config.Network.IpRouteConfig.DefaultGateway
         $dvpg = Get-VirtualPortGroup -Distributed -Name "hello"
         $dvpgvid = $dvpg.ExtensionData.Config.DefaultPortConfig.Vlan.VlanId
@@ -348,12 +358,27 @@ function testGnarly{
         #this is to get primary and secondary dns servers and search domain
         $dnses = $currHost | Get-VMHostNetwork | select dnsaddress, searchdomain
         $dns = $dnses.dnsaddress
-        $primaryDNS = $dns[0]
-        $secondaryDNS = $dns[1]
+        if($dns.length -eq 2){
+            $primaryDNS = $dns[0]
+            $secondaryDNS = $dns[1]
+        } else {
+            $primaryDNS = $dns
+            $secondaryDNS = " "
+        }
+        
+        #this is to get time configurations
+        $ntpserver = $currHost | Get-VMHostNtpServer
+        #$ntpserver
 
+        #this is to get authentication services
+        $dom = Get-VMHostAuthentication -VMHost $currHost
+        #$dom.domain
 
+        #this is to get the host profile
+        $hostProfile = $currHost | Get-VMHostProfile
+        #$hostProfile.name
 
-        Write-Output "hostname: $($currHost.name) `nadaptername: $($mgmtIP.name) `nipaddress: $($mgmtIP.ip) `nsubnetmask: $($mgmtIP.subnetmask) `ndefaultgateway: $($defaultGateway) `nvladid: $($dvpgvid) `nprimaryDNS: $($primaryDNS) `nsecondaryDNS: $($secondaryDNS) `n`n"
+        Write-Output "hostname: $($currHost.name) `nadaptername: $($mgmtIP.name) `nipaddress: $($mgmtIP.ip) `nsubnetmask: $($mgmtIP.subnetmask) `ndefaultgateway: $($defaultGateway) `nip6address1: $($primaryip6) `nip6address2: $($secondaryip6) `nvladid: $($dvpgvid) `nprimaryDNS: $($primaryDNS) `nsecondaryDNS: $($secondaryDNS) `n`n"
     }
 }
 
